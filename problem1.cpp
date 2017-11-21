5fbca021a5ef4250705afebc57e5cd7d019dd5ec
hasan169 <shagoraust@gmail.com>
2017-11-21 19:23:02 +0600

    first commit

---
+++ problem1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+#include<queue>
+#include<cstring>
+#include<vector>
+#include<set>
+#include<map>
+#include<cstdio>
+#include<cmath>
+#include<algorithm>
+using namespace std;
+int main(){
+
+    string str;
+    getline(cin,str); // input
+    if(str == "Who loves Byang?")cout<<"Byangette"<<endl; // Byangette loves Byang
+    else cout<<"Byang"<<endl;
+    return 0;
+}
